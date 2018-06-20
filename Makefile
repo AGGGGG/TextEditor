c58edeb0015326ebd5441f5e94a40a7b2c8959bf
AndrewJGaut <ajg@umail.ucsb.edu>
2018-06-19 17:36:27 -0700

    added some new flags and outputs characters

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-CC = cc
+CC = gcc
 
 CCFLAGS = -Wall -g -std=c99
 
+BINARIES = editor
 
 editor: editor.o
 	$(CC) $(CCFLAGS) -o $@ $^
+
+clean:
+	/bin/rm -f *.o $(BINARIES)
--- editor.c
+++ editor.c
@@ -2,15 +2,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <ctype.h> //like cctype (for character operations)
+#include <stdio.h> //for printf
 
+//make this variable global so can use anywhere
+//because not sure how to pass paramters to fucntion pointer
+//when calling atexit
+struct termios orig_settings;
+
+
+//restore terminal to original settings by saving the original values
+//and setting it to them when the program exits
+void disable_raw_mode()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_settings);
+}
 void enable_raw_mode()
 {
 	struct termios raw;
 
 	tcgetattr(STDIN_FILENO, &raw);
-	raw.c_lflag &= ~(ECHO);
+	orig_settings = raw; //save original settings
+
+	raw.c_lflag &= ~(ECHO); //so you can't see what you type
+	raw.c_lflag &= ~(ICANON); //so the progam read input byte-by-byte (not when user presses enter)
+	raw.c_lflag &= ~(ISIG); //so that ctrl-c and ctrl-z (and any other ctrls) don't send signals anymore
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+	atexit(disable_raw_mode); //make sure that original settings restored when program exits
 }
+
+
 
 int main()
 {
@@ -18,6 +40,25 @@ int main()
 
 	char c;
 	//now, read in until either 1) there's an error or 2) a q is typed
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	//in the loop, print out characters
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	{
+		if(iscntrl(c))
+		{
+			//not a readable character
+			printf("%d\n", c);
+		} 
+		else
+		{
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
+
+
+
+
+
+
+
